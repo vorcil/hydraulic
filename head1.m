9bee173a1835f71423fee86da73887752a46e1c5
vorcil <rajol.kochlashvili@mail.ru>
2015-08-02 21:10:15 +1200

    updated hydrostatic condition check

--- head1.m
+++ head1.m
@@ -7,6 +7,9 @@ global g=9.820;
 global GM=9.86*(10^14);
 %fluid density - default filtered water
 global rho=1.0;
+%velocity of fluid in aquifier/other
+global v=0;
+global condition_v="null";
 
 %calculate bernoulli equation in terms of pressure
 pressure_head=calcP();
@@ -15,4 +18,11 @@ function calcP(){
 %solidpipe v perforation pipe = default null 1.0
 P=1.0;
 return P/(rho*g);
+}
+
+
+function hydrostatic(){
+    if v==0{
+        condition_v="hydrostatic conditions";
+    }
 }
