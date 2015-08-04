6119c2c1971b62a7ab6a33258a4b86372fa30872
vorcil <rajol.kochlashvili@mail.ru>
2015-08-04 18:42:39 +1200

    Head2

--- head1.m
+++ head1.m
@@ -62,3 +62,88 @@ function hydrostatic(){
         condition_v="hydrostatic conditions";
     }
 }
+
+setMethod(f="initialize",
+          signature="piper",
+          definition=function(.Object, l, call=sys.call(), size=NULL,
+              group=NULL, colours=NULL, numbersymbols=FALSE,
+              wt.col=NULL, wt.pch=NULL, pt.col=NULL)
+          {
+            # initialise member data
+            if ( !is.null(size) ) {
+              .Object@size <- size
+            } else {
+                .Object@size <- 1
+            }
+            .Object@Ca <- l$Ca
+            .Object@Mg <- l$Mg
+            .Object@Cl <- l$Cl
+            .Object@SO4 <- l$SO4
+            .Object@pt.pch <- l$pt.pch
+            if ( !is.null(l$group) ) {
+                .Object@group <- group
+            } else { # plot all samples on the same plot
+                .Object@group <- rep( 1, times=length(l$Ca) )
+            }
+            if ( !is.null(l$WaterType) ) {
+                .Object@WaterType <- l$WaterType
+            } else { # treat each sample as an individual water type
+                .Object@WaterType <- seq( 1:length(row.names(l)) )
+            }
+            if ( !is.null(l$IDs) ) {
+                .Object@IDs <- l$IDs
+            } else {
+                .Object@IDs <- row.names(l)
+            }
+            # TODO: make this less confusing
+            # make the colours and symbols for the points now
+            # because it saves confusion later
+            # wt.col and wt.pch should be factors (?)
+            # pt.col and pt.pch should be vectors
+            # pt. overides wt.
+          
+            # set pt.col
+            if ( ! is.null( l$pt.col ) ) {              # if specified
+              .Object@pt.col <- l$pt.col                # assign it
+            } else {                                    # else calculate it...
+              wtf <- as.factor(.Object@WaterType)
+              if ( ! is.null( l$wt.col ) ) {
+                if ( length(l$wt.col) != length( levels(wtf) ) ) {
+                  cat("ERROR: wt.col wrong length for WaterType!")
+                  return(invisible())
+                } else { levels(wtf) <- l$wt.col }
+              } else {  # the default for pt.col
+                levels( wtf ) <- seq( 1:length(levels(wtf)) )
+              }
+                  
+              .Object@pt.col <- as.vector( wtf )
+            }
+
+            # set pt.pch
+            # input wt:      2 2 1 2 3
+            # output pt.col  2 2 1 2 3
+            #        pt.pch  1 2 1 3 1
+            if ( ! is.null( l$pt.pch ) ) {             # if specified
+              .Object@pt.pch <- l$pt.pch               # assign it
+            } else {                                   # else calculate it...
+              wtf <- as.factor(.Object@WaterType)
+              pch <- .Object@WaterType                 # initialise
+              ## if ( ! is.null( l$wt.pch ) ) {
+##               for ( i in levels(wtf) ) {
+##                  # get subset , replace with values from vector
+##                  lrp<-sum( wtf==i ) # the number of samples of that watertype
+##                  pch[ wtf==i ] <- l$wt.pch[1:lrp]
+##                }
+##              } else {
+##                # loop through levels
+##                for ( i in levels(wtf) ) {
+##                  lrp<-sum( wtf==i ) # the number of samples of that watertype
+##                  pch[ wtf==i ] <- seq(lrp)
+##                }
+##              }
+           ###   .Object@pt.pch <- pch                 # assign
+          }
+            .Object@call <- call
+              return(.Object)
+          }
+)
